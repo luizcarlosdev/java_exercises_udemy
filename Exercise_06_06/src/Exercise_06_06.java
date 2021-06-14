2c9c5a2e456addce8a5a86534fd345ce3e430e5e
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2021-06-14 15:41:58 -0300

    Exercise 06_06 for - average

---
+++ Exercise_06_06/src/Exercise_06_06.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise_06_06 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			double n1 = sc.nextDouble();
+			double n2 = sc.nextDouble();
+			double n3 = sc.nextDouble();
+			
+			double media = ((n1 * 2) + (n2 * 3) + (n3 * 5)) / 10;
+			
+			System.out.printf("%.1f\n", media);
+		}
+		
+		sc.close();
+
+	}
+
+}
