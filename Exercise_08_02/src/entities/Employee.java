aea9278c8947b8b24f067cd9cc4bc42e5f0b936f
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2021-06-14 22:26:20 -0300

    Exercise 08_02 OO - classes

---
+++ Exercise_08_02/src/entities/Employee.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Employee {
+	
+	public String name;
+	public double grossSalary;
+	public double tax;
+	
+	public double NetSalary() {
+		return grossSalary - tax;
+	}
+	
+	public double IncreaseSalary(double percentage) {
+		return grossSalary * (1 + percentage / 100) - tax;
+	}
+
+}
