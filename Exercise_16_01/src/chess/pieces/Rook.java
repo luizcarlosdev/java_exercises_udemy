4d8d409a3569b0b14841dbe5be05e302ef5c8b9c
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2021-07-05 15:13:04 -0300

    Exercise 16_01 - Chess text mode Implementing possible moves of Rook

--- Exercise_16_01/src/chess/pieces/Rook.java
+++ Exercise_16_01/src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -9,15 +10,62 @@ public class Rook extends ChessPiece {
 	public Rook(Board board, Color color) {
 		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "R";
 	}
-	
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position position = new Position(0, 0);
+
+		// check above positions
+		position.setValues(this.position.getRow() - 1, this.position.getColumn());
+		while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+			position.setRow(position.getRow() - 1);
+		}
+
+		if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+		}
+
+		// check left positions
+		position.setValues(this.position.getRow(), this.position.getColumn() - 1);
+		while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+			position.setColumn(position.getColumn() - 1);
+		}
+
+		if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+		}
+
+		// check right positions
+		position.setValues(this.position.getRow(), this.position.getColumn() + 1);
+		while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+			position.setColumn(position.getColumn() + 1);
+		}
+
+		if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+		}
+
+		// check below positions
+		position.setValues(this.position.getRow() + 1, this.position.getColumn());
+		while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+			position.setRow(position.getRow() + 1);
+		}
+
+		if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+		}
+
 		return matrix;
 	}
 
