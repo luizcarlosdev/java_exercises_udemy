879002821dba9725fbc6c47942d85d61a2969eb7
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2021-07-06 14:23:37 -0300

    Exercise 16_01 - Chess text mode Switching player each turn

--- Exercise_16_01/src/application/Program.java
+++ Exercise_16_01/src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -30,7 +30,7 @@ public class Program {
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				
 				System.out.println();
-				System.out.println("Target: ");
+				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
