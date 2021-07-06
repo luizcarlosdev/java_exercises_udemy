d974ccc4f83c239cc8991a19f1224af119310129
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2021-07-06 14:06:44 -0300

    Exercise 16_01 - Chess text mode Implementing possible moves of King

--- Exercise_16_01/src/chess/pieces/King.java
+++ Exercise_16_01/src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -9,15 +10,72 @@ public class King extends ChessPiece {
 	public King(Board board, Color color) {
 		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "K";
 	}
-	
+
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+
+		return p == null || p.getColor() != getColor();
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// check above position
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+
+		// check below position
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+
+		// check left position
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+
+		// check right position
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+
+		// check northwest position
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+
+		// check northeast position
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+
+		// check south-west position
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+
+		// check south-east position
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+
 		return matrix;
 	}
 
