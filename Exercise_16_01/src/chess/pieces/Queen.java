f4d7c72715fb67d27b45ac166d1a7a34f586c74f
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2021-07-07 15:00:44 -0300

    Exercise 16_01 - Chess text mode Queen

---
+++ Exercise_16_01/src/chess/pieces/Queen.java
@@ -0,0 +1,116 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece {
+
+	public Queen(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position position = new Position(0, 0);
+
+		// check above positions
+		position.setValues(this.position.getRow() - 1, this.position.getColumn());
+		while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+			position.setRow(position.getRow() - 1);
+		}
+
+		if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+		}
+
+		// check left positions
+		position.setValues(this.position.getRow(), this.position.getColumn() - 1);
+		while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+			position.setColumn(position.getColumn() - 1);
+		}
+
+		if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+		}
+
+		// check right positions
+		position.setValues(this.position.getRow(), this.position.getColumn() + 1);
+		while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+			position.setColumn(position.getColumn() + 1);
+		}
+
+		if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+		}
+
+		// check below positions
+		position.setValues(this.position.getRow() + 1, this.position.getColumn());
+		while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+			position.setRow(position.getRow() + 1);
+		}
+
+		if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+		}
+		
+		// check northwest positions
+				position.setValues(this.position.getRow() - 1, this.position.getColumn() - 1);
+				while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+					matrix[position.getRow()][position.getColumn()] = true;
+					position.setValues(position.getRow() - 1, position.getColumn() - 1);
+				}
+
+				if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+					matrix[position.getRow()][position.getColumn()] = true;
+				}
+
+				// check northeast positions
+				position.setValues(this.position.getRow() - 1, this.position.getColumn() + 1);
+				while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+					matrix[position.getRow()][position.getColumn()] = true;
+					position.setValues(position.getRow() - 1, position.getColumn() + 1);
+				}
+
+				if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+					matrix[position.getRow()][position.getColumn()] = true;
+				}
+
+				// check south-east positions
+				position.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
+				while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+					matrix[position.getRow()][position.getColumn()] = true;
+					position.setValues(position.getRow() + 1, position.getColumn() + 1);
+				}
+
+				if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+					matrix[position.getRow()][position.getColumn()] = true;
+				}
+
+				// check south-west positions
+				position.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+				while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+					matrix[position.getRow()][position.getColumn()] = true;
+					position.setValues(position.getRow() + 1, position.getColumn() - 1);
+				}
+
+				if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+					matrix[position.getRow()][position.getColumn()] = true;
+				}
+
+		return matrix;
+	}
+	
+	@Override
+	public String toString() {
+		return "Q";
+	}
+
+}
